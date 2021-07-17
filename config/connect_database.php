1c7ef5b08951f5a7803262b74518942b77106363
maitrongnhan001 <maitrongnhan.001@gmail.com>
2021-07-17 17:24:07 +0700

    crate theme place html

---
+++ config/connect_database.php
@@ -0,0 +1,33 @@
+<?php
+define('HOST','localhost');
+define('USERNAME', 'root');
+define('PASSWORD', '');
+define('DATABASE', 'food-order');
+define('SITEURL', 'http://localhost/food-order/');
+session_start();
+
+function execute($sql) {
+    echo('call');
+    $conn = mysqli_connect(HOST, USERNAME, PASSWORD, DATABASE);
+    if($conn -> connect_error) {
+        die();
+    }
+    $result = mysqli_query($conn, $sql);
+    $conn -> close();
+    return $result;
+}
+
+function executeResult($sql) {
+    $conn = mysqli_connect(HOST, USERNAME, PASSWORD, DATABASE);
+    if($conn -> connect_error) {
+        return $conn -> connect_error;
+    }
+    $result = mysqli_query($conn, $sql);
+    $data = array();
+    while($row = mysqli_fetch_array($result, 1)){
+        $data[] = $row;
+    }
+    $conn -> close();
+    return $data;
+}
+?>
