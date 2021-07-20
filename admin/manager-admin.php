4af56700402f7fdc9c86946c94ad18159adcb45f
maitrongnhan001 <maitrongnhan.001@gmail.com>
2021-07-20 22:04:17 +0700

    added function update, delete, change password

--- admin/manager-admin.php
+++ admin/manager-admin.php
@@ -5,9 +5,20 @@
         <h1>Manager Admin</h1>
         <br />
         <?php
+        //check session create admin
         if (isset($_SESSION['add'])) {
-            echo $_SESSION['add'].'<br /><br /><br />';
+            echo $_SESSION['add'] . '<br /><br /><br />';
             unset($_SESSION['add']);
+        }
+        //check session delete admin
+        if (isset($_SESSION['delete'])) {
+            echo $_SESSION['delete'] . '<br /><br /><br />';
+            unset($_SESSION['delete']);
+        }
+        //check session update admin
+        if (isset($_SESSION['update'])) {
+            echo $_SESSION['update']. '<br /><br /><br />';
+            unset($_SESSION['update']);
         }
         ?>
         <a href="add-admin.php" class="btn-primary">Add Admin</a>
@@ -19,52 +30,28 @@
                 <th>Username</th>
                 <th>Action</th>
             </tr>
-            <tr>
-                <td>1</td>
-                <td>Mai Trong Nhan</td>
-                <td>TrongNhan</td>
-                <td>
-                    <a class="btn-secondary">Update Admin</a>
-                    <a class="btn-danger">Delete Admin</a>
-                </td>
-            </tr>
 
             <?php
             $query = "SELECT * FROM tbl_admin";
             $result = executeResult($query);
-            foreach($result as $index) {
-                echo ("
-                    <tr>
-                        <td>".$index['id']."</td>
-                        <td>".$index['full_name']."</td>
-                        <td>".$index['username']."</td>
-                        <td>
-                            <a class='btn-secondary'>Update Admin</a>
-                            <a class='btn-danger'>Delete Admin</a>
-                        </td>
-                    </tr>
-                ");
+            foreach ($result as $index) {
+                $Delete_Url = SITEURL . 'admin/delete-admin.php?id=' . $index['id'];
+                $Update_Url = SITEURL . 'admin/update-admin.php?id=' . $index['id'];
+                $ChangePass_Url = SITEURL . 'admin/update-password.php?id=' . $index['id'];
+            ?>
+                <tr>
+                    <td><?php echo $index['id'] ?></td>
+                    <td><?php echo $index['full_name'] ?></td>
+                    <td><?php echo $index['username'] ?></td>
+                    <td>
+                        <a class='btn-primary' href=<?php echo $ChangePass_Url ?>>Update Password</a>
+                        <a class='btn-secondary' href=<?php echo $Update_Url ?>>Update Admin</a>
+                        <a class='btn-danger' href=<?php echo $Delete_Url ?>>Delete Admin</a>
+                    </td>
+                </tr>
+            <?php
             }
             ?>
-
-            <tr>
-                <td>1</td>
-                <td>Mai Trong Nhan</td>
-                <td>TrongNhan</td>
-                <td>
-                    <a class="btn-secondary">Update Admin</a>
-                    <a class="btn-danger">Delete Admin</a>
-                </td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>Mai Trong Nhan</td>
-                <td>TrongNhan</td>
-                <td>
-                    <a class="btn-secondary">Update Admin</a>
-                    <a class="btn-danger">Delete Admin</a>
-                </td>
-            </tr>
         </table>
     </div>
 </div>
