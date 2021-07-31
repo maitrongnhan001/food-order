be8bebae2b4d038a443de56175cbcf2e2ec57493
maitrongnhan001 <maitrongnhan.001@gmail.com>
2021-07-31 21:56:26 +0700

    added function order food

--- order.php
+++ order.php
@@ -1,4 +1,25 @@
-<?php include('Layout/menu.php'); ?>
+<?php include('Layout/menu.php');
+if (isset($_GET['id'])) {
+    //Get food id
+    $food_id = $_GET['id'];
+    //Get data of with food id from database
+    $query = "SELECT * FROM tbl_food WHERE id = $food_id";
+    $result = executeResult($query);
+    if (count($result) == 1) {
+        //Get data from database
+        $title = $result[0]['title'];
+        $price = $result[0]['price'];
+        $image_name = $result[0]['image_name'];
+    } else {
+        //Food is not availabe
+        //Redirect to home page
+        header('Location: ' . SITEURL);
+    }
+} else {
+    //don't get food id
+    header('Location: ' . SITEURL);
+}
+?>
 
 <!-- fOOD sEARCH Section Starts Here -->
 <section class="food-search">
@@ -6,17 +27,28 @@
 
         <h2 class="text-center text-white">Fill this form to confirm your order.</h2>
 
-        <form action="#" class="order">
+        <form action="#" class="order" method="POST">
             <fieldset>
                 <legend>Selected Food</legend>
 
                 <div class="food-menu-img">
-                    <img src="images/menu-pizza.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                    <?php
+                    if ($image_name != "") {
+                    ?>
+                        <img src=<?php echo SITEURL . 'images/food/' . $image_name; ?> alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                    <?php
+                    } else {
+                        echo '<div class="error">No Image</div>';
+                    }
+                    ?>
                 </div>
 
                 <div class="food-menu-desc">
-                    <h3>Food Title</h3>
-                    <p class="food-price">$2.3</p>
+                    <h3> <?php echo $title; ?> </h3>
+                    <p class="food-price"> <?php echo $price; ?> </p>
+
+                    <input type="hidden" name="food" value= <?php echo $title; ?> >
+                    <input type="hidden" name="price" value= <?php echo $price; ?> >
 
                     <div class="order-label">Quantity</div>
                     <input type="number" name="qty" class="input-responsive" value="1" required>
@@ -48,4 +80,62 @@
 </section>
 <!-- fOOD sEARCH Section Ends Here -->
 
-<?php include('Layout/footer.php'); ?>
+<?php 
+require_once('./Debug/Debug.php');
+if (isset($_POST['submit'])) {
+    //Get data food
+    $food = $_POST['food'];
+    $price = $_POST['price'];
+    $qty = $_POST['qty'];
+
+    $total = $price * $qty;
+
+    $order_date = date('Y-m-d h:i:sa');
+
+    $status = 'Ordered';
+
+    $customer_name = $_POST['full-name'];
+    $customer_contact = $_POST['contact'];
+    $customer_email = $_POST['email'];
+    $customer_address = $_POST['address'];
+    //store data to database
+    $sql = "INSERT INTO tbl_order 
+    (
+        food,
+        price,
+        qty,
+        total,
+        order_date,
+        status,
+        customer_name,
+        customer_contact,
+        customer_email,
+        customer_address
+    ) VALUES 
+    (
+        '$food',
+        '$price',
+        $qty,
+        $total,
+        '$order_date',
+        '$status',
+        '$customer_name',
+        '$customer_contact',
+        '$customer_email',
+        '$customer_address'
+    )";
+    // debug($sql);
+    $result = execute($sql);
+    
+    //check execute database
+    if ($result) {
+        $_SESSION['order'] = '<div class="success text-center">Food Ordered Successfully</div>';
+        header('Location: '.SITEURL);
+    } else {
+        $_SESSION['order'] = '<div class="error text-center">Faild to Order Food</div>';
+        header('Location: '.SITEURL);
+    }
+}
+ 
+include('Layout/footer.php'); 
+?>
