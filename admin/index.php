26bc23a057a0cf3d4a946ea2f909fca545e1ca34
maitrongnhan001 <maitrongnhan.001@gmail.com>
2021-08-01 21:10:22 +0700

    added function display information of producer

--- admin/index.php
+++ admin/index.php
@@ -3,32 +3,49 @@
     <div class="main-content">
         <div class="wrapper">
             <h1>Dashboard</h1>
+            <div class="center">
             <div class="col-4 text-center">
-                <h1>5</h1>
+                <?php
+                //count catagory from database
+                $query = "SELECT count(id) as member_id FROM tbi_category";
+                $result = executeResult($query);
+                ?>
+                <h1> <?php echo $result[0]['member_id']; ?> </h1>
                 <br/>
                 Categogys
             </div>
             <div class="col-4 text-center">
-                <h1>5</h1>
+            <?php
+                //count catagory from database
+                $query = "SELECT count(id) as member_id FROM tbl_food";
+                $result = executeResult($query);
+                ?>
+                <h1> <?php echo $result[0]['member_id']; ?> </h1>
                 <br/>
-                Categogys
+                Foods
             </div>
             <div class="col-4 text-center">
-                <h1>5</h1>
+            <?php
+                //count catagory from database
+                $query = "SELECT count(id) as member_id FROM tbl_order";
+                $result = executeResult($query);
+                ?>
+                <h1> <?php echo $result[0]['member_id']; ?> </h1>
                 <br/>
-                Categogys
+                Total Order
             </div>
             <div class="col-4 text-center">
-                <h1>5</h1>
+            <?php
+                //count catagory from database
+                $query = "SELECT sum(total) as Total FROM tbl_order WHERE status = 'Delivered'";
+                $result = executeResult($query);
+                ?>
+                <h1> <?php echo $result[0]['Total']; ?> </h1>
                 <br/>
-                Categogys
-            </div>
-            <div class="col-4 text-center">
-                <h1>5</h1>
-                <br/>
-                Categogys
+                Revenue Generated
             </div>
             <div class="clear-fix"></div>
+            </div>
         </div>
     </div>
     <!--main content section end-->
