baaeed46c5cfdbbf3913e3f5515fdf06074a66c9
maitrongnhan001 <maitrongnhan.001@gmail.com>
2021-08-03 21:43:24 +0700

    fix link in menu

--- View/Layout/menu.php
+++ View/Layout/menu.php
@@ -26,16 +26,16 @@ include('../Model/connect_database.php');
             <div class="menu text-right">
                 <ul>
                     <li>
-                        <a href= <?php echo SITEURL.'index.php'; ?>>Home</a>
+                        <a href= <?php echo SITEURL.'View/index.php'; ?>>Home</a>
                     </li>
                     <li>
-                        <a href= <?php echo SITEURL.'categories.php'; ?>>Categories</a>
+                        <a href= <?php echo SITEURL.'View/categories.php'; ?>>Categories</a>
                     </li>
                     <li>
-                        <a href= <?php echo SITEURL.'foods.php'; ?>>Foods</a>
+                        <a href= <?php echo SITEURL.'View/foods.php'; ?>>Foods</a>
                     </li>
                     <li>
-                        <a href=<?php echo SITEURL.'admin' ?>>Admin</a>
+                        <a href=<?php echo SITEURL.'View/admin' ?>>Admin</a>
                     </li>
                 </ul>
             </div>
